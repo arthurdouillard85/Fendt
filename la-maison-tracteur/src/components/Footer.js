525b4e34756a44ab383d5ab8b0843cf81a2ef495
Arthur <arthur.douillard85@gmail.com>
2024-04-19 17:03:50 +0200

    Modification u footer pour coherence

--- la-maison-tracteur/src/components/Footer.js
+++ la-maison-tracteur/src/components/Footer.js
@@ -2,32 +2,12 @@ import { useState } from "react";
 import "../styles/Footer.css";
 
 function Footer() {
-  const [inputValue, setInputValue] = useState("");
-
-  function handleInput(e) {
-    setInputValue(e.target.value);
-  }
-
-  function handleBlur() {
-    if (!inputValue.includes("@")) {
-      alert(
-        "Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥"
-      );
-    }
-  }
 
   return (
     <footer className="lmt-footer">
       <div className="lmt-footer-elem">
-        Pour les passionné·e·s de tracteurs 🚜
+        Pour les passionné·e·s de Fendt 🚜
       </div>
-      <div className="lmt-footer-elem">Laissez-nous votre mail :</div>
-      <input
-        placeholder="Entrez votre mail"
-        onChange={handleInput}
-        value={inputValue}
-        onBlur={handleBlur}
-      />
     </footer>
   );
 }
